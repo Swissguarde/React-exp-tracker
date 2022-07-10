8344e407b630e2ac0b50caa2e42af19d98e9499c
Kolade Ayomide <ayomikolade2003@gmail.com>
2022-07-10 20:57:39 +0100

    fix tansaction color code

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -6,7 +6,7 @@ const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
-    <li className="minus">
+    <li className={transaction.amount > 0 ? "plus" : "minus"}>
       {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
