ecadf7552c2fef9401b6021ccd25629898de5459
Kolade Ayomide <ayomikolade2003@gmail.com>
2022-07-10 20:28:29 +0100

    first commit

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  // Get the sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className="minus">
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
+
+export default Transaction;
---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,19 @@
+const reduce = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
+};
+export default reduce;
